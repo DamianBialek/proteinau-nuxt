8e21055bdc8a090a9236a1692428a7ca4fa3e113
Damian <damianbiaek@hotmail.com>
2018-08-05 15:16:46 +0200

    Add CRUD to articles | Production api

--- config/index.js
+++ config/index.js
@@ -5,14 +5,24 @@
 //     articlesFromCategory: 'https://bialko.000webhostapp.com/articles/'
 // }
 
+// export const url = {
+//     articles: 'http://localhost/cms_api/articles',
+//     article: 'http://localhost/cms_api/article/',
+//     addArticle: 'http://localhost/cms_api/article/add',
+//     editArticle: 'http://localhost/cms_api/article/edit/',
+//     deleteArticle: 'http://localhost/cms_api/article/delete',
+//     categories: 'http://localhost/cms_api/categories',
+//     articlesFromCategory: 'http://localhost/cms_api/articles/'
+// }
+
 export const url = {
-    articles: 'http://localhost/cms_api/articles',
-    article: 'http://localhost/cms_api/article/',
-    addArticle: 'http://localhost/cms_api/article/add',
-    editArticle: 'http://localhost/cms_api/article/edit/',
-    deleteArticle: 'http://localhost/cms_api/article/delete',
-    categories: 'http://localhost/cms_api/categories',
-    articlesFromCategory: 'http://localhost/cms_api/articles/'
+    articles: 'https://bialko.000webhostapp.com/articles',
+    article: 'https://bialko.000webhostapp.com/article/',
+    addArticle: 'https://bialko.000webhostapp.com/article/add',
+    editArticle: 'https://bialko.000webhostapp.com/article/edit/',
+    deleteArticle: 'https://bialko.000webhostapp.com/article/delete',
+    categories: 'https://bialko.000webhostapp.com/categories',
+    articlesFromCategory: 'https://bialko.000webhostapp.com/articles/'
 }
 
 export const projectConfig = {
